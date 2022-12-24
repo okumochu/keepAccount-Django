e37afa6e804ee51c80a600269467115008255604
okumochu <okumochu55@gmail.com>
2022-12-24 08:44:25 +0800

    neww

--- keepAccount/models.py
+++ keepAccount/models.py
@@ -1,16 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
+TYPE_CHOICES = (
+    ("1", "食"),
+    ("2", "衣"),
+    ("3", "住"),
+    ("4", "行"),
+    ("5", "育"),
+    ("6", "樂"),
+)
+
 class account(models.Model):
-    userName=models.CharField(max_length=128)
-    disciption=models.CharField(max_length=128)
+    user=models.ForeignKey(User,blank=True,null=True, on_delete=models.CASCADE)
+    disciption=models.TextField(blank=True)
     money=models.IntegerField()
-    date=models.DateField()
-    type=models.IntegerField()
+    date=models.DateTimeField()
+    type=models.CharField(max_length = 20,choices = TYPE_CHOICES,default = '1')
 
     def __str__(self):
-        return f'{self.userName} spend {self.money} on {self.type}  {self.disciption} at {self.date}' 
+        return f'{self.user} spends {self.money} on {self.type}  {self.disciption} at {self.date}' 
 
 
 
